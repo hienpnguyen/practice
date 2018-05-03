6462893da3f18a05e53ee428a309e779b91187ba
hienng2 <hienng2@cisco.com>
2018-05-02 21:11:56 -0700

    Extra problems

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,20 @@
+import LeetCode.Solution;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        int[] nums = {1, 2, 3};
+
+        System.out.println(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+
+        List<List<Integer>> list = s.permute(nums);
+
+        System.out.println(list);
+
     }
 }
--- src/test/java/MyClassTest.java
+++ src/test/java/MyClassTest.java
@@ -1,5 +1,7 @@
 import Sorting.BubbleSort;
 import Sorting.MergeSort;
+import LeetCode.Solution;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,24 +12,43 @@ public class MyClassTest {
     public void bubbleSortTest() {
         BubbleSort bubbleSort = new BubbleSort();
         List<Integer> a = Arrays.asList(5, 1, 4, 2, 8);
-
-        System.out.println(a);
+        List<Integer> expectedResult = Arrays.asList(1, 2, 4, 5, 8);
 
         List<Integer> b = bubbleSort.bubbleSort(a);
 
-        System.out.println(b);
+        Assert.assertEquals(expectedResult, b);
     }
 
     @Test
     public void mergeSortTest(){
         int arr[] = {12, 11, 13, 5, 6, 7};
+        String expectedResult = "[5, 6, 7, 11, 12, 13]";
 
         MergeSort mergeSort = new MergeSort();
 
-        System.out.println(Arrays.toString(arr));
-
         mergeSort.sort(arr, 0, arr.length - 1);
 
-        System.out.println(Arrays.toString(arr));
+        Assert.assertEquals(expectedResult, Arrays.toString(arr));
+    }
+
+    @Test
+    public void reverse(){
+        int x = 123;
+        int expectedResult = 321;
+
+        Solution s = new Solution();
+
+        Assert.assertEquals(expectedResult, s.reverse(x));
+    }
+
+    @Test
+    public void isPalindrome(){
+        int x = 121;
+        boolean expectedResult = true;
+
+        Solution s = new Solution();
+
+        Assert.assertEquals(expectedResult, s.isPalindrome(x));
+
     }
 }
